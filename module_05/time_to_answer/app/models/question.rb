1ae2dfe97485aad96d7f41109d9864d0a17838eb
DaniloIS <danilosantos-pg@hotmail.com>
2022-05-24 11:04:05 -0300

    Filtrando as questões

--- module_05/time_to_answer/app/models/question.rb
+++ module_05/time_to_answer/app/models/question.rb
@@ -5,4 +5,20 @@ class Question < ApplicationRecord
 
   # Kaminari
   paginates_per 5
+
+  scope :_search_subject_, ->(page, subject_id) {
+    includes(:answers, :subject)
+    .where(subject_id: subject_id)
+    .page(page)
+  }
+
+  scope :_search_, ->(page, term) {
+    includes(:answers, :subject)
+    .where("lower(description) LIKE ?", "%#{term.downcase}%")
+    .page(page)
+  }
+
+  scope :last_questions, ->(page) {
+    includes(:answers, :subject).order('created_at desc').page(page)
+  }
 end
